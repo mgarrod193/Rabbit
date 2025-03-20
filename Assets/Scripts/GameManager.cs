ca256866f1f6d5b6907cb1c70fc2d68b477b0802
mgarrod <mgarrod193@gmail.com>
2025-03-20 12:51:19 +0000

    Initial setup of game manager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //variables to manage gamestate
+    private int _lives = 3;
+    private gameState _state;
+
+    //reference to player prefab and spawnpoint
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private GameObject _playerSpawnPoint;
+
+
+    private void PlayerDied()
     {
-        
+        DecreaseLives();
+        if (_lives >0 )
+        { Instantiate(_playerSpawnPoint, _playerSpawnPoint.transform.position, _playerSpawnPoint.transform.rotation);  }
+        else
+        {
+            UpdateGameState(gameState.Lose);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DecreaseLives()
     {
-        
+        _lives--;
+    }
+
+
+    private void UpdateGameState(gameState GameState)
+    {
+        _state = GameState;
+    }
+
+    private enum gameState
+    {
+        MainMenu,
+        Gameplay,
+        Win,
+        Lose,
     }
 }
